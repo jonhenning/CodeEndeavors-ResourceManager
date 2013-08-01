04206210ad7bda5c5c5cd686e751d4d8875e8deb
Jon Henning <jhenning@endeavorii>
2013-08-01 17:16:32 -0500

    enabling nuget

--- CodeEndeavors.ResourceManager.Test/Program.cs
+++ CodeEndeavors.ResourceManager.Test/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
-using StructureMap;
+using CodeEndeavors.Extensions;
+//using StructureMap;
 
 namespace CodeEndeavors.ResourceManager.Test
 {
@@ -11,36 +12,61 @@ namespace CodeEndeavors.ResourceManager.Test
     {
         static void Main(string[] args)
         {
+            //using (var repo = new ResourceRepository("RavenDB", ResourceRepository.RepositoryType.RavenDb))
+            //{
 
-            using (var repo = new ResourceRepository("RavenDB", ResourceRepository.RepositoryType.RavenDb))
+            //var connection = "{ type:'AzureBlob', azureBlobStorage:'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==' }";
+            var connection = "{ type:'AzureBlob', azureBlobStorage:'dev' }";
+
+            using (var repo = new ResourceRepository(connection))
             {
-                //foreach (var o in repo.Get<string>())
-                //{
-                //    repo.Purge(o);
-                //}
 
-                var resource = repo.Get<string>("foo3");
-                if (resource == null)
-                {
-                    resource = new DomainObjects.Resource<string>()
-                    {
-                        Key = "foo3",
-                        Sequence = 1
-                    };
-                }
-                else
-                {
-                    Console.WriteLine("Already Exists! " + resource.Audit.Count);
-                }
-                resource = JsonConvert.DeserializeObject<DomainObjects.Resource<string>>(JsonConvert.SerializeObject(resource));
-                //resource.Key = "foo3";
-                repo.Save(resource, 3);
+                var type = typeof(MyObj).ToString();
 
-                Console.WriteLine(JsonConvert.SerializeObject(repo.Get<string>()));
+                var resources = repo.GetResources<MyObj>(type);
+                Console.WriteLine(resources.ToJson());
+
+                var o = new MyObj() { Text = "foo" };
+                var userId = "1";
+                repo.DeleteAll<string>(type);
+
+
+                var resource = new DomainObjects.Resource<MyObj>()
+                {
+                    Type = type,
+                    Sequence = 1,
+                    Data = o
+                };
+
+                resource = repo.StoreResource(resource, userId);
+                repo.SaveChanges();
+
+                var r = repo.GetResourceById<MyObj>(resource.Id);
+                Console.WriteLine(r.ToJson());
+
+                resources = repo.GetResources<MyObj>(type);
+                Console.WriteLine(resources.ToJson());
+
+                repo.Delete<MyObj>(resource);
+                repo.SaveChanges();
+
+                resources = repo.GetResources<MyObj>(type);
+                Console.WriteLine(resources.ToJson());
+            
             }
+
+
             Console.WriteLine("DONE!");
             Console.ReadLine();
 
         }
+
+        public class MyObj
+        {
+            public string Id { get; set; }
+            public string Text { get; set; }
+        }
+
+
     }
 }
