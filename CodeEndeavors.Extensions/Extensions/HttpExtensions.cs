278a6072802793e4bd266dba02e3f254e0b83d5b
Jon Henning <jhenning@endeavorii>
2013-10-23 13:47:28 -0500

    removed HttpExtensions.ReadAsFileAsync which referenced .NET 2.0 System.Net.Http assembly...   if needed will add a nuget ref to Microsoft.Net.Http package

--- CodeEndeavors.Extensions/Extensions/HttpExtensions.cs
+++ CodeEndeavors.Extensions/Extensions/HttpExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
-using System.Net.Http;
+//using System.Net.Http;
 using System.Net;
 
 namespace CodeEndeavors.Extensions
@@ -32,33 +32,33 @@ namespace CodeEndeavors.Extensions
             //}
         }
 
-        public static Task ReadAsFileAsync(this HttpContent content, string filename, bool overwrite)
-        {
-            var pathname = Path.GetFullPath(filename);
-            if (!overwrite && File.Exists(filename))
-            {
-                throw new InvalidOperationException(string.Format("File {0} already exists.", pathname));
-            }
+        //public static Task ReadAsFileAsync(this HttpContent content, string filename, bool overwrite)
+        //{
+        //    var pathname = Path.GetFullPath(filename);
+        //    if (!overwrite && File.Exists(filename))
+        //    {
+        //        throw new InvalidOperationException(string.Format("File {0} already exists.", pathname));
+        //    }
 
-            FileStream fileStream = null;
-            try
-            {
-                fileStream = new FileStream(pathname, FileMode.Create, FileAccess.Write, FileShare.None);
-                return content.CopyToAsync(fileStream).ContinueWith(
-                    (copyTask) =>
-                    {
-                        fileStream.Close();
-                    });
-            }
-            catch
-            {
-                if (fileStream != null)
-                {
-                    fileStream.Close();
-                }
+        //    FileStream fileStream = null;
+        //    try
+        //    {
+        //        fileStream = new FileStream(pathname, FileMode.Create, FileAccess.Write, FileShare.None);
+        //        return content.CopyToAsync(fileStream).ContinueWith(
+        //            (copyTask) =>
+        //            {
+        //                fileStream.Close();
+        //            });
+        //    }
+        //    catch
+        //    {
+        //        if (fileStream != null)
+        //        {
+        //            fileStream.Close();
+        //        }
 
-                throw;
-            }
-        }
+        //        throw;
+        //    }
+        //}
     }
 }
