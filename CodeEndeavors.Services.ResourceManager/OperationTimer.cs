1754a51d735ce6fcc746ac725cb331e696074f18
Jon Henning <Jon Henning>
2017-04-27 10:53:20 -0500

    ENH: OperationTimer

---
+++ CodeEndeavors.Services.ResourceManager/OperationTimer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEndeavors.Services.ResourceManager
+{
+    public class OperationTimer : IDisposable
+    {
+        private Stopwatch _watch = new Stopwatch();
+        private string _message;
+
+        public OperationTimer(string message)
+            : this(message, null)
+        {
+        }
+        public OperationTimer(string message, params object[] args)
+        {
+            if (args != null)
+                _message = string.Format(message, args);
+            else
+                _message = message;
+            _watch.Start();
+        }
+
+        public void Dispose()
+        {
+            _watch.Stop();
+            ServiceHost.Common.Services.Logging.Info(string.Format("TIMER:  {0} : {1}ms", _message, _watch.ElapsedMilliseconds));
+            GC.SuppressFinalize(this);
+        }
+    }
+}
